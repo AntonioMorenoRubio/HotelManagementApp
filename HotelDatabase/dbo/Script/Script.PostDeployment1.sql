5fda1b397ca15408af548d70c0abddf2efe0a0a9
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2023-03-22 19:10:17 +0100

    SQL Server Database Deployment

---
+++ HotelDatabase/dbo/Script/Script.PostDeployment1.sql
@@ -0,0 +1,38 @@
+﻿/*
+Plantilla de script posterior a la implementación							
+--------------------------------------------------------------------------------------
+ Este archivo contiene instrucciones de SQL que se anexarán al script de compilación.		
+ Use la sintaxis de SQLCMD para incluir un archivo en el script posterior a la implementación.			
+ Ejemplo:      :r .\miArchivo.sql								
+ Use la sintaxis de SQLCMD para hacer referencia a una variable en el script posterior a la implementación.		
+ Ejemplo:      :setvar TableName miTabla							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+if not exists (select 1 from dbo.RoomTypes)
+begin
+    INSERT INTO dbo.RoomTypes (Title, Description, Price) VALUES
+    ('King Size Bed','This is a room with one king-size bed and a window.', 100),
+    ('Two Queen Size Beds','This is a room with two queen-size beds and one window.', 115),
+    ('Exective Suite', 'Two rooms, each with a king-size bed and a window.', 205);
+end
+
+if not exists (select 1 from dbo.Rooms)
+begin
+    declare @roomId1 int;
+    declare @roomId2 int;
+    declare @roomId3 int;
+
+    select @roomId1 = Id from dbo.RoomTypes WHERE Title = 'King Size Bed'
+    select @roomId2 = Id from dbo.RoomTypes WHERE Title = 'Two Queen Size Beds'
+    select @roomId3 = Id from dbo.RoomTypes WHERE Title = 'Exective Suite'
+
+    INSERT INTO dbo.Rooms (RoomNumber, RoomTypeId) VALUES 
+    ('101', @roomId1),
+    ('102', @roomId1),
+    ('103', @roomId1),
+    ('201', @roomId2),
+    ('202', @roomId2),
+    ('301', @roomId3);
+end
---
+++ HotelDatabase/dbo/Tables/Reservations.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Reservations]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [ClientId] INT NOT NULL, 
+    [RoomId] INT NOT NULL,
+    [StartDate] DATETIME2 NOT NULL, 
+    [EndDate] DATETIME2 NOT NULL, 
+    [ClientHasCheckedIn] BIT NOT NULL DEFAULT 0, 
+    [TotalCost] MONEY NOT NULL, 
+    CONSTRAINT [FK_Reservations_Clients] FOREIGN KEY ([ClientId]) REFERENCES Clients(Id), 
+    CONSTRAINT [FK_Reservations_Rooms] FOREIGN KEY ([RoomId]) REFERENCES Rooms(Id)
+)
